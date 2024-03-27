a91c2c324c019284d2481ce955a570dd0e16cf76
Brian Zee <brianzee2004@gmail.com>
2024-03-27 15:14:31 -0700

    install and set up

--- myApp.js
+++ myApp.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+require('mongoose');
 
+mongoose.connect("mongodb+srv://user-freeCode:TeamFire32123!@cluster0.g2yowoa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", 
+{ useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
